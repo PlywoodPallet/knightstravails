57d4ee7a6cddf232a26dbdfeb3732c72761ce86f
PlywoodPallet <25418374+PlywoodPallet@users.noreply.github.com>
2022-07-19 16:18:42 -0700

    Abstract Board#all_possible_knight moves to take any tree as input. *Untested* Debugging requires a method to print trees first

--- lib/board.rb
+++ lib/board.rb
@@ -3,73 +3,78 @@ require './lib/tree.rb'
 
 class Board
 
-attr_accessor :grid
+  attr_accessor :grid
 
-def initialize
-  @grid = Array.new(8){Array.new(8)} # initialize empty 8x8 chess grid
-end
+  def initialize
+    @grid = Array.new(8){Array.new(8)} # initialize empty 8x8 chess grid
+  end
 
-# add a piece to the board, in position [0][0] by default
-def add_piece(piece, x, y)
-  @grid[x][y] = piece
-end
+  # add a piece to the board, in position [0][0] by default
+  def add_piece(piece, x, y)
+    @grid[x][y] = piece
+  end
 
-# added knight moves to the Board class, but it might need to be moved. Does the Board class move a chess piece, or does the piece move itself? (thinking ahead for future chess project)
+  # added knight moves to the Board class, but it might need to be moved. Does the Board class move a chess piece, or does the piece move itself? (thinking ahead for future chess project)
 
-# all possible knight moves on (x,y) grid - relative move to starting position
-# (-2, -1), (-1, 2), (1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1)
+  # all possible knight moves on (x,y) grid - relative move to starting position
+  # (-2, -1), (-1, 2), (1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1)
 
-# if after hypothetical move, the resulting index is negative (x or y), delete the node because the move is off the board
+  # if after hypothetical move, the resulting index is negative (x or y), delete the node because the move is off the board
 
-# test input. Start = [0,0], finish = [1,2]
+  # test input. Start = [0,0], finish = [1,2]
 
-def knight_moves (piece, start, finish)
-
-
-
-
-
-
-end
-
-# possible moves
-def possible_knight_moves (piece, start, finish)
-  # all the possible moves for one player turn (x,y grid)
-  possible_moves_array = [[-2, 1], [-1, 2], [1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1]]
-
-  moves_tree = Tree.new(start)
-
-
-  
-
-  possible_moves_array.each do |coord|
-    start_x = start[0]
-    start_y = start[1]
-
-    coord_x = coord[0]
-    coord_y = coord[1]
-
-    finish_x = start_x + coord_x
-    finish_y = start_y + coord_y
-
-    # check if move is off the board: skip if the resulting coordinates are negative
-    if (finish_x < 0 || finish_y < 0)
-      next
-    # check if move is off the board: skip if the resulting coordinates are greater than the board size. Assuming board is square
-    elsif (finish_x > @grid.length || finish_y > @grid.length)
-      next
-    # of checks are passed, add this valid possible move to moves_tree
-    else
-      finish_array = [finish_x, finish_y]
-
-      moves_tree.root.add_child(TreeNode.new(finish_array))
-    end
-
-
+  def knight_moves (piece, start, finish)
 
   end
 
-  p moves_tree
-end
+  # possible moves
+  def all_possible_knight_moves (start, finish)
+    # all the possible moves for one player turn (x,y grid)
+    possible_moves_array = [[-2, 1], [-1, 2], [1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1]]
+
+    moves_tree = Tree.new(start)
+
+    final_move_found = false
+
+    # until final_move_found
+
+      leaf_nodes = moves_tree.return_leafs
+
+      
+      leaf_nodes.each do |leaf_node|
+        possible_moves_array.each do |coord|
+          # local start and finish, for the move only
+          local_start_x = leaf_node.value[0]
+          local_start_y = leaf_node.value[1]
+
+          coord_x = coord[0]
+          coord_y = coord[1]
+
+          local_finish_x = local_start_x + coord_x
+          local_finish_y = local_start_y + coord_y
+
+          # check if move is off the board: skip if the resulting coordinates are negative
+          if (local_finish_x < 0 || local_finish_y < 0)
+            next
+          # check if move is off the board: skip if the resulting coordinates are greater than the board size. Assuming board is square
+          elsif (local_finish_x > @grid.length || local_finish_y > @grid.length)
+            next
+          # of checks are passed, add this valid possible move to moves_tree
+          else
+            local_finish_array = [local_finish_x, local_finish_y]
+
+            # check if the last move has been found
+            if (local_finish_array == finish)
+              final_move_found = true
+            end
+
+            leaf_node.add_child(TreeNode.new(local_finish_array))
+          end
+        end
+      end
+    # end
+
+    moves_tree
+  end
 
 end
--- testing.rb
+++ testing.rb
@@ -3,23 +3,9 @@ require './lib/knight.rb'
 require './lib/tree.rb'
 
 
-# a_knight = Knight.new
-# test_board = Board.new
+a_knight = Knight.new
+test_board = Board.new
 
-# test_board.add_piece(a_knight, 0, 0)
+test_board.add_piece(a_knight, 0, 0)
 
-# test_board.knight_moves(a_knight, [0,0], [1,2])
-
-# a_treenode = TreeNode.new("test")
-# puts a_treenode
-
-a_tree = Tree.new("root")
-a_tree.root.add_child(TreeNode.new("child1"))
-a_tree.root.add_child(TreeNode.new("child2"))
-a_tree.root.add_child(TreeNode.new("child3"))
-a_tree.root.add_child(TreeNode.new("child4"))
-
-puts a_tree.return_leafs
-
-# a_graph = Graph.new
-# p a_graph
+puts test_board.all_possible_knight_moves([0,0], [1,2])
